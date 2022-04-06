c654caf6080196b86789c1b13636a0d906561591
abigcatc683 <abigcatc683@SDS-L-763HC63>
2022-04-06 14:47:18 -0400

    Add project files.

---
+++ Conversions/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Conversions
+{
+    partial class Conversions
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.optionsLabel = new System.Windows.Forms.Label();
+            this.conversionChoiceLabel = new System.Windows.Forms.Label();
+            this.conversionChoiceInput = new System.Windows.Forms.TextBox();
+            this.conversionValueLabel = new System.Windows.Forms.Label();
+            this.conversionValueInput = new System.Windows.Forms.TextBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.titleLabel.Location = new System.Drawing.Point(132, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(309, 40);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Linear Conversion";
+            // 
+            // optionsLabel
+            // 
+            this.optionsLabel.AutoSize = true;
+            this.optionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.optionsLabel.Location = new System.Drawing.Point(163, 91);
+            this.optionsLabel.Name = "optionsLabel";
+            this.optionsLabel.Size = new System.Drawing.Size(222, 100);
+            this.optionsLabel.TabIndex = 1;
+            this.optionsLabel.Text = "1. Inches to Centimetres\r\n2. Feet to Centimetres\r\n3. Yards to Metres\r\n4. Miles to" +
+    " Kilometres";
+            // 
+            // conversionChoiceLabel
+            // 
+            this.conversionChoiceLabel.AutoSize = true;
+            this.conversionChoiceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.conversionChoiceLabel.Location = new System.Drawing.Point(100, 258);
+            this.conversionChoiceLabel.Name = "conversionChoiceLabel";
+            this.conversionChoiceLabel.Size = new System.Drawing.Size(227, 20);
+            this.conversionChoiceLabel.TabIndex = 2;
+            this.conversionChoiceLabel.Text = "Enter conversion choice (1 - 4):";
+            // 
+            // conversionChoiceInput
+            // 
+            this.conversionChoiceInput.Location = new System.Drawing.Point(353, 258);
+            this.conversionChoiceInput.Name = "conversionChoiceInput";
+            this.conversionChoiceInput.Size = new System.Drawing.Size(100, 20);
+            this.conversionChoiceInput.TabIndex = 3;
+            // 
+            // conversionValueLabel
+            // 
+            this.conversionValueLabel.AutoSize = true;
+            this.conversionValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.conversionValueLabel.Location = new System.Drawing.Point(104, 296);
+            this.conversionValueLabel.Name = "conversionValueLabel";
+            this.conversionValueLabel.Size = new System.Drawing.Size(207, 20);
+            this.conversionValueLabel.TabIndex = 4;
+            this.conversionValueLabel.Text = "Enter value to be converted:";
+            // 
+            // conversionValueInput
+            // 
+            this.conversionValueInput.Location = new System.Drawing.Point(353, 296);
+            this.conversionValueInput.Name = "conversionValueInput";
+            this.conversionValueInput.Size = new System.Drawing.Size(100, 20);
+            this.conversionValueInput.TabIndex = 5;
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(108, 341);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(345, 23);
+            this.convertButton.TabIndex = 6;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.outputLabel.ForeColor = System.Drawing.Color.Red;
+            this.outputLabel.Location = new System.Drawing.Point(135, 367);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(289, 38);
+            this.outputLabel.TabIndex = 7;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Conversions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.conversionValueInput);
+            this.Controls.Add(this.conversionValueLabel);
+            this.Controls.Add(this.conversionChoiceInput);
+            this.Controls.Add(this.conversionChoiceLabel);
+            this.Controls.Add(this.optionsLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Conversions";
+            this.Text = "Conversions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label optionsLabel;
+        private System.Windows.Forms.Label conversionChoiceLabel;
+        private System.Windows.Forms.TextBox conversionChoiceInput;
+        private System.Windows.Forms.Label conversionValueLabel;
+        private System.Windows.Forms.TextBox conversionValueInput;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Conversions/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversions
+{
+    public partial class Conversions : Form
+    {
+        public Conversions()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            double value = Convert.ToDouble(conversionValueInput.Text);
+            string choice = conversionChoiceInput.Text;
+            if (choice == "1")
+            {
+                double answer = InchesToCm(value);
+                outputLabel.Text = $"{value} inches = {answer} cm";
+            }
+            else if (choice == "2")
+            {
+                double answer = FeetToCm(value);
+                outputLabel.Text = $"{value} feet = {answer} cm";
+            }
+            else if (choice == "3")
+            {
+                double answer = YardsToMetres(value);
+                outputLabel.Text = $"{value} yards = {answer} metres";
+            }
+            else
+            {
+                double answer = MilesToKm(value);
+                outputLabel.Text = $"{value} miles = {answer} km";
+            }
+        }
+
+        public double InchesToCm(double inches)
+        {
+            double cm = inches * 2.54;
+            return cm;
+        }
+
+        public double FeetToCm(double feet)
+        {
+            double cm = feet * 30.48;
+            return cm;
+        }
+
+        public double YardsToMetres(double yards)
+        {
+            double metres = yards * 0.91;
+            return metres;
+        }
+        
+        public double MilesToKm(double miles)
+        {
+            double km = miles * 1.6;
+            return km;
+        }
+    }
+}
